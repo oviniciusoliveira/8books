d854991c980d84d319c25e54d46f4ac0cc6aab6b
Vinicius Oliveira <viniciuso.contato@gmail.com>
2022-01-28 12:27:31 -0300

    feat: create Home page UI and load posts from prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
 import { GetStaticProps } from 'next';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Prismic from '@prismicio/client';
+import { Document } from '@prismicio/client/types/documents';
 
+import ApiSearchResponse from '@prismicio/client/types/ApiSearchResponse';
+import { useState } from 'react';
+import { format } from 'date-fns';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -16,7 +22,7 @@ interface Post {
 }
 
 interface PostPagination {
-  next_page: string;
+  next_page?: string;
   results: Post[];
 }
 
@@ -24,13 +30,103 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+function mapResults(results: Document[]): Post[] {
+  const posts = results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'd MMM y'
+      ),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  return posts;
+}
 
-//   // TODO
-// };
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const [posts, setPosts] = useState(postsPagination.results);
+  const [nextPage, setNextPage] = useState<string | null>(
+    postsPagination.next_page
+  );
+  const [isLoadingMorePosts, setIsLoadingMorePosts] = useState(false);
+
+  async function getNextPage(): Promise<void> {
+    setIsLoadingMorePosts(true);
+    try {
+      const responseRaw = await fetch(nextPage);
+      const responseJson: ApiSearchResponse = await responseRaw.json();
+      const results = mapResults(responseJson.results);
+      setPosts(prevState => [...prevState, ...results]);
+      setNextPage(responseJson.next_page);
+    } catch {
+      return;
+    } finally {
+      setIsLoadingMorePosts(false);
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <img src="/images/logo.svg" alt="logo" />
+      </header>
+      <main>
+        {posts.map(post => {
+          return (
+            <div key={post.uid} className={styles.post}>
+              <h2>{post.data.title}</h2>
+              <p>{post.data.subtitle}</p>
+              <div className={styles.postInfo}>
+                <span>
+                  <FiCalendar /> {post.first_publication_date}
+                </span>
+                <span>
+                  <FiUser /> {post.data.author}
+                </span>
+              </div>
+            </div>
+          );
+        })}
+
+        {nextPage && (
+          <button
+            className={styles.button}
+            type="button"
+            onClick={() => getNextPage()}
+          >
+            {isLoadingMorePosts ? 'Carregando...' : 'Carregar mais posts'}
+          </button>
+        )}
+      </main>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const prismic = getPrismicClient();
+  const response = await prismic.query(
+    Prismic.predicates.at('document.type', 'posts'),
+    {
+      pageSize: 1,
+    }
+  );
+
+  const nextPage = response.next_page;
+
+  const results = mapResults(response.results);
+
+  return {
+    props: {
+      postsPagination: {
+        next_page: nextPage,
+        results,
+      },
+    },
+  };
+};
