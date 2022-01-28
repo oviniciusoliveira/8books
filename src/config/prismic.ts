ee64ab5d36049b6028fd0134d76bf6e0d61c69a0
Vinicius Oliveira <viniciuso.contato@gmail.com>
2022-01-28 12:27:06 -0300

    chore: config prismic

---
+++ src/config/prismic.ts
@@ -0,0 +1,5 @@
+const repoName = '8books';
+
+export const apiEndpoint = `https://${repoName}.prismic.io/api/v2`;
+
+export const accessToken = process.env.PRISMIC_ACCESS_TOKEN;
--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,9 +1,11 @@
 import Prismic from '@prismicio/client';
 import { DefaultClient } from '@prismicio/client/types/client';
+import { accessToken, apiEndpoint } from '../config/prismic';
 
 export function getPrismicClient(req?: unknown): DefaultClient {
-  const prismic = Prismic.client(process.env.PRISMIC_API_ENDPOINT, {
+  const prismic = Prismic.client(apiEndpoint, {
     req,
+    accessToken,
   });
 
   return prismic;
